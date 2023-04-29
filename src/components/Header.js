3bbd50b7fee67b0bd132c7699c83da91491a5053
Himanshu <himvarandani@gmail.com>
2023-04-29 23:59:13 +0530

    Add font awesome setup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,5 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Header = () => {
   return (
@@ -6,7 +8,15 @@ const Header = () => {
         <div className="w-full flex flex-row justify-between items-center px-40 py-5">
           <div className="flex flex-row items-center">
             <img src="/logo.png" />
-            <input type="text" placeholder="Search Events" className="bg-gray-550 rounded-xl h-8 pl-8 ml-5" />
+            <div className="relative">
+              <FontAwesomeIcon
+                icon={faSearch}
+                size="lg"
+                color="white"
+                className="absolute top-1.5 pl-6"
+              />
+              <input type="text" placeholder="Search Events" className="bg-gray-550 rounded-xl h-8 pl-8 ml-5" />
+            </div>
           </div>
           <div className="flex flex-row items-center text-lg text-gray-650 font-medium">
             <p className="mr-5">About</p>
