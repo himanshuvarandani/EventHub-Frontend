97b02f01b8bc079ab66ff26b49382e3e69b76998
Himanshu <himvarandani@gmail.com>
2023-04-30 04:58:21 +0530

    Update login component

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,27 +3,23 @@ const Login = () => {
     <div className="h-screen w-full bg-purple-550 py-20 px-40">
       <div className="flex justify-between h-full">
         <div className="bg-white rounded-xl w-2/5 h-full">
-          <img src="/Image3.png" className="w-full rounded-t-xl" />
-          <div className="flex flex-col items-center my-10">
-            <img src="/logo.png" className="h-12" />
-            <div className="text-2xl font-medium mt-5">
-              <p className="pl-16 pr-32">Experience...</p>
-              <p className="pl-32 pr-16">Unforgettable</p>
-              <p className="pl-48 pr-0">Events...!!</p>
+          <img src="/Image3.png" className="h-1/4 w-full rounded-t-xl" />
+          <div className="flex flex-col justify-center h-3/4 py-10">
+            <div className="flex flex-col items-center">
+              <img src="/logo.png" className="h-12" />
+              <div className="text-2xl font-medium my-3">
+                <p className="pl-16 pr-32">Experience...</p>
+                <p className="pl-32 pr-16">Unforgettable</p>
+                <p className="pl-48 pr-0">Events...!!</p>
+              </div>
             </div>
+            <hr className="border-y-2 border-solid border-gray-650 mx-5" />
+            <p className="text-center text-xl my-3 mx-10">
+              We @EventHUB helps you to find perfect event
+              in your city to attend and you can create
+              and publish your own event on same platform!!
+            </p>
           </div>
-          <hr className="border-y-2 border-solid border-gray-650 mx-5" />
-          <p className="text-center text-xl m-10">
-            We @EventHUB
-            <br />
-            helps you to find perfect event
-            <br />
-            in your city to attend and you can create
-            <br />
-            and publish your own event
-            <br />
-            on same platform!!
-          </p>
         </div>
         <div className="bg-white rounded-xl w-1/2 h-full py-10">
           <div className="text-3xl text-center font-bold">
